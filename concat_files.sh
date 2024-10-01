05b80a242c8939806a0398af679769096ff81b6f
nblank02 <blanknj@mail.uc.edu>
2024-10-01 16:55:02 -0400

    Commit Bash Project

--- concat_files.sh
+++ concat_files.sh
@@ -1,95 +1,84 @@
 #!/bin/bash
 
-# Display help/usage information
-function display_help() {
-    echo "Usage: $0 [options] file1 file2 ... fileN"
-    echo
+# Function to display usage
+display_usage() {
+    echo "Usage: ./concat_files.sh [options] file1 file2 ..."
+    echo ""
     echo "Options:"
-    echo "  -o <output_file>  Specify the output file name (required)"
+    echo "  -o <output_file>  Specify the output file name (default: output.txt)"
     echo "  -h                Display this help message"
-    echo
+    echo ""
     echo "Example:"
-    echo "  $0 -o result.txt file1.txt file2.txt"
+    echo "  ./concat_files.sh -o output.txt file1.txt file2.txt"
 }
 
-# Check for valid .txt files using a regular expression
-function is_valid_file() {
-    local file=$1
-    if [[ $file =~ ^.*\.txt$ ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
+# Default output file
+output_file="output.txt"
 
-# Handle invalid arguments or no arguments
-if [[ $# -eq 0 ]]; then
-    echo "Error: No arguments provided."
-    display_help
-    exit 1
+# Default input files
+default_files=("file1.txt" "file2.txt" "file3.txt")
+
+# Check if any arguments are provided
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. Using default files."
+    input_files=("${default_files[@]}")
+else
+    # Process options and arguments
+    while getopts "o:h" opt; do
+        case $opt in
+            o) output_file="$OPTARG" ;;
+            h) display_usage; exit 0 ;;
+            *) echo "Invalid option: -$OPTARG" >&2; display_usage; exit 1 ;;
+        esac
+    done
+    shift $((OPTIND - 1))
+
+    # If input files were provided as arguments
+    input_files=("$@")
 fi
 
-# Initialize variables
-output_file=""
-input_files=()
-
-# Parse options and arguments
-while getopts ":o:h" option; do
-    case $option in
-        o)
-            output_file=$OPTARG
-            ;;
-        h)
-            display_help
-            exit 0
-            ;;
-        \?)
-            echo "Invalid option: -$OPTARG" >&2
-            display_help
-            exit 1
-            ;;
-        :)
-            echo "Option -$OPTARG requires an argument." >&2
-            display_help
-            exit 1
-            ;;
-    esac
-done
-
-shift $((OPTIND - 1))
-
-# Ensure output file is specified
-if [[ -z $output_file ]]; then
-    echo "Error: Output file is required."
-    display_help
-    exit 1
-fi
-
-# Collect input files
-for file in "$@"; do
-    if is_valid_file "$file"; then
-        if [[ -f $file ]]; then
-            input_files+=("$file")
-        else
-            echo "Warning: File $file does not exist. Skipping."
-        fi
+# Validate input files
+valid_input_files=()
+for file in "${input_files[@]}"; do
+    if [[ -f "$file" ]]; then
+        valid_input_files+=("$file")
     else
-        echo "Warning: File $file is not a valid text file. Skipping."
+        echo "Warning: File $file does not exist. Skipping."
     fi
 done
 
-# Check if there are any valid input files
-if [[ ${#input_files[@]} -eq 0 ]]; then
+# If no valid input files were provided, check the default files
+if [ ${#valid_input_files[@]} -eq 0 ]; then
+    valid_input_files=("${default_files[@]}")
+    echo "Using default files: ${valid_input_files[@]}"
+fi
+
+# Validate default input files
+for file in "${valid_input_files[@]}"; do
+    if [[ ! -f "$file" ]]; then
+        echo "Warning: File $file does not exist. Skipping."
+        valid_input_files=("${valid_input_files[@]/$file}")
+    fi
+done
+
+# Check if there are valid input files
+if [ ${#valid_input_files[@]} -eq 0 ]; then
     echo "Error: No valid text files to concatenate."
     exit 1
 fi
 
-# Concatenate the files into the output file
+# Concatenate files
 echo "Concatenating files..."
-> "$output_file"  # Clear the output file
-for file in "${input_files[@]}"; do
+# Debug output: list of valid input files
+echo "Valid input files: ${valid_input_files[@]}"
+
+# Clear the output file before writing
+: > "$output_file"
+
+# Loop through each valid input file and append its contents to the output file
+for file in "${valid_input_files[@]}"; do
     cat "$file" >> "$output_file"
-    echo "" >> "$output_file"  # Add a new line between files
+    echo "Added contents of $file to $output_file."
 done
 
 echo "Files successfully concatenated into $output_file."
